1aac046aeef2fd75ea1eec9a38bf035daa6d5af2
thoth000 <snym719a@gmail.com>
2020-04-29 00:06:03 +0900

    このグラフでよさげ

--- lib/pages/chart.dart
+++ lib/pages/chart.dart
@@ -8,12 +8,24 @@ class ChartsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('チャート'),
       ),
-      body: Center(child: Chart()),
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: <Widget>[
+          Chart(Colors.blue,500.0,'青'),
+          Chart(Colors.green,200.0,'緑'),
+          Chart(Colors.amber,700.0,'アンバー'),
+          Chart(Colors.red,900,'赤'),
+        ],
+      ),
     );
   }
 }
 
 class Chart extends StatelessWidget {
+  Color color;
+  double number;
+  String title;
+  Chart(this.color,this.number,this.title);
   @override
   Widget build(BuildContext context) {
     final GlobalKey<AnimatedCircularChartState> _chartKey =
@@ -22,21 +34,28 @@ class Chart extends StatelessWidget {
     List<CircularStackEntry> data = <CircularStackEntry>[
       new CircularStackEntry(
         <CircularSegmentEntry>[
-          new CircularSegmentEntry(500.0, Colors.red[200], rankKey: 'Q1'),
-          new CircularSegmentEntry(1000.0, Colors.green[200], rankKey: 'Q2'),
-          new CircularSegmentEntry(2000.0, Colors.blue[200], rankKey: 'Q3'),
-          new CircularSegmentEntry(1000.0, Colors.yellow[200], rankKey: 'Q4'),
+          new CircularSegmentEntry(number, color, rankKey: 'Q1'),
+          new CircularSegmentEntry(1000-number, Colors.grey[400], rankKey: 'Q2'),
         ],
         rankKey: 'Quarterly Profits',
       ),
     ];
-    return AnimatedCircularChart(
-      key: _chartKey,
-      size: Size(150, 150),
-      initialChartData: data,
-      chartType: CircularChartType.Radial,
-      edgeStyle: SegmentEdgeStyle.round,
-      percentageValues: false,
+    return Column(
+      children: <Widget>[
+        AnimatedCircularChart(
+          key: _chartKey,
+          size: Size(150, 150),
+          duration: Duration(seconds: 1),
+          initialChartData: data,
+          chartType: CircularChartType.Radial,
+          holeRadius: 50,
+          holeLabel: '100%',
+          labelStyle: TextStyle(fontSize: 30,color: Colors.black,fontWeight: FontWeight.w700),
+          edgeStyle: SegmentEdgeStyle.round,
+          percentageValues: false,
+        ),
+        Text(title,style: TextStyle(fontSize: 20,fontWeight: FontWeight.w700),),
+      ],
     );
   }
 }
